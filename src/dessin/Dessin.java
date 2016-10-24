f168d3ace930a71939bcacb041863a5f7e2df55f
Simon <Simon@LAPTOP-EH706Q0V>
2016-10-24 15:08:26 -0400

    initialisation

---
+++ src/dessin/Dessin.java
@@ -0,0 +1,14 @@
+package dessin;
+
+/**
+ *
+ * @author Simon
+ */
+public class Dessin {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
